2ef1ac0ee4e915003afce427de80b489d1400e58
viclen <victor.lengler@gmail.com>
2022-07-24 17:30:52 -0300

    fix: app root

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Músicas JEPA</title>
+        <meta name="description" content="Aplicativo de músicas da JEPA" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
